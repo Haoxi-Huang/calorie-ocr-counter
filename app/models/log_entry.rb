7500bcaa05d51d4d73b748ff050d009bd2b4c8df
Haoxi Huang <haoxi98@gmail.com>
2023-11-06 21:43:03 -0500

    Add data structures to support future updates

---
+++ app/models/log_entry.rb
@@ -0,0 +1,3 @@
+class LogEntry < ApplicationRecord
+  belongs_to :daily_log
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_04_055728) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_024125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "daily_logs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "log_entries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
