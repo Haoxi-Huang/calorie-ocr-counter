1b852726592753dceed9d0c7d91416a46ed5bd63
Haoxi Huang <haoxi98@gmail.com>
2023-11-21 23:56:55 -0500

    Basic CRUD for daily logs and log entries

--- app/controllers/daily_logs_controller.rb
+++ app/controllers/daily_logs_controller.rb
@@ -1,2 +1,17 @@
 class DailyLogsController < ApplicationController
+  before_action :set_daily_log, except: :index
+
+  def index
+    @daily_logs = current_user.daily_logs
+  end
+
+  def new
+    @daily_log = DailyLog.new
+  end
+
+  private
+
+  def set_daily_log
+    @daily_log = DailyLog.find_by(params[:id])
+  end
 end
